d347d26e7ace9e5e717076676b8602fc18774b84
TanzeelaBilal <tansheikh32@gmail.com>
2023-07-10 16:41:32 +0500

    TestCases Integrated

---
+++ app/src/test/java/com/assessment/photoslibrary/data/remote/RemoteDataSourceTest.kt
@@ -0,0 +1,54 @@
+package com.assessment.photoslibrary.data.remote
+
+import com.assessment.photoslibrary.model.response.PhotoModel
+import com.assessment.photoslibrary.model.response.PhotosListResponse
+import com.assessment.photoslibrary.model.response.PhotosMetaData
+import kotlinx.coroutines.runBlocking
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
+import retrofit2.Response
+
+@RunWith(MockitoJUnitRunner::class)
+class RemoteDataSourceTest {
+    @Mock
+    lateinit var apiService: ApiService
+
+    @InjectMocks
+    lateinit var remoteDataSource: RemoteDataSource
+
+    @Test
+    fun `test getPhotos success`() {
+        val photosListResponse =
+            PhotosListResponse(PhotosMetaData("1", "10", "100", "1000", listOf<PhotoModel>()))
+        val response = Response.success(photosListResponse)
+        Mockito.`when`(runBlocking { apiService.getPhotosList() }).thenReturn(response)
+
+        val result = runBlocking { remoteDataSource.getPictures() }
+
+        assertNotNull(result)
+        assertEquals(response, result)
+    }
+
+    @Test
+    fun `test getUser failure`() {
+        val response = Response.error<PhotosListResponse>( 404, ResponseBody.create("application/json".toMediaTypeOrNull(), ""))
+
+
+        Mockito.`when`(runBlocking { apiService.getPhotosList() }).thenReturn(response)
+
+        val result = runBlocking { remoteDataSource.getPictures() }
+
+        assertEquals(response, result)
+    }
+}
---
+++ app/src/test/java/com/assessment/photoslibrary/utils/UtilsTest.kt
@@ -0,0 +1,12 @@
+package com.assessment.photoslibrary.utils
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class UtilsTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
