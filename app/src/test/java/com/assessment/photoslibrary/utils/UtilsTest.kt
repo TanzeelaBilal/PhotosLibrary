fd658cbf84009ffa94343e3ffb775885f504cd62
TanzeelaBilal <tansheikh32@gmail.com>
2023-07-11 10:19:16 +0500

    TestCases Integrated

--- app/src/test/java/com/assessment/photoslibrary/utils/UtilsTest.kt
+++ app/src/test/java/com/assessment/photoslibrary/utils/UtilsTest.kt
@@ -1,12 +1,30 @@
 package com.assessment.photoslibrary.utils
 
-import org.junit.Test
-
+import com.assessment.photoslibrary.model.response.PhotoModel
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.spy
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class UtilsTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun `test getImageUrl`() {
+        val photo = mock(PhotoModel::class.java)
+        val utils = spy(Utils)
+
+        `when`(photo.farm).thenReturn("66")
+        `when`(photo.server).thenReturn("65535")
+        `when`(photo.id).thenReturn("48837804368")
+        `when`(photo.secret).thenReturn("709b44acf8")
+
+        val imageUrl = utils.getImageUrl(photo)
+
+        val expectedUrl = "https://farm66.staticflickr.com/65535/48837804368_709b44acf8.jpg"
+        assertEquals(expectedUrl, imageUrl)
     }
 }
